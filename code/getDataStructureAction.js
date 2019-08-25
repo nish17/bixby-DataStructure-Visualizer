66d8ea50dc3653a8be88f6eb74301fe3b68a40ef
nish17 <nimesh3536@gmail.com>
2019-08-26 02:40:51 +0530

    fix typo

--- code/getDataStructureAction.js
+++ code/getDataStructureAction.js
@@ -3,7 +3,7 @@ var console = require('console');
 module.exports.function = function getDataStructureAction (dsName) {
   let info = "";
    data.forEach(ds => {
-     if(ds.name == dataStructure){ 
+     if(ds.name.toLowerCase() == dsName.toLowerCase()){ 
        info = JSON.stringify(ds);
        console.log(info);
        }
