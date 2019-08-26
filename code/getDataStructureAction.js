a90cd7737016c2ce4dfdf51e3ef3fbdedbc2cfd1
nish17 <nimesh3536@gmail.com>
2019-08-26 17:23:35 +0530

    currently it works without proper view

--- code/getDataStructureAction.js
+++ code/getDataStructureAction.js
@@ -1,12 +1,29 @@
 var data = require('./utils/dataStructure.js');
 var console = require('console');
-module.exports.function = function getDataStructureAction (dsName) {
+module.exports.function = function getDataStructureAction(dsName, type) {
+  // var ds = null;
+  var dsArray = [];
   let info = "";
-   data.forEach(ds => {
-     if(ds.name.toLowerCase() == dsName.toLowerCase()){ 
-       info = JSON.stringify(ds);
-       console.log(info);
-       }
- });
- return info;
+
+  if (dsName) {
+
+    data.forEach(ds => {
+      if (ds.name.toLowerCase() == dsName.toLowerCase()) dsArray.push(ds)
+    });
+
+    if (dsArray.length > 0) {
+      return dsArray;
+    }
+  }
+
+  if (type) {
+    data.forEach(ds => {
+      if (ds.type.toLowerCase() == type.toLowerCase()) dsArray.push(ds)
+    });
+
+    if (dsArray.length > 0) {
+      return dsArray;
+    }
+  }
+  return data;
 }
